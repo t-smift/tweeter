adf30fe10ced67b0865596ac5a5d99c906ce849c
Taylor Smith <taylor.l.smith07@gmail.com>
2022-10-12 18:53:41 -0700

    added ajax code to submit new tweets to database and render without refresh

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,73 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+const escape = function (str) {
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
+//function to change the submitted tweet into "Safe Text"
+
+const createTweetElement = function (data) {
+  const tweetTime = timeago.format(data.created_at); 
+  const tweetBody = escape(data.content.text)
+  const result = `<article><header><span class="topTweetLine"><span class="tweeter-name"><img src="/images/profile-hex.png"><p>${data.user.name}</p></span><p>${data.user.handle}</p></span><p>${tweetBody}</p></header><footer class="tweetFeet"><span><h5>${tweetTime}</h5></span><span class="icons"><i class="fa-solid fa-flag"></i><i class="fa-solid fa-heart"></i><i class="fa-solid fa-retweet"></i></span></footer></article>`
+  return result;
+};
+
+const renderTweets = function (tweets) {
+  for (let tweet of tweets) {
+    const $tweet = createTweetElement(tweet);
+    $('.tweets').prepend($tweet)
+
+  }
+};
+
+const loadTweets = function () {
+  $.ajax({
+    url: '/tweets',
+    method: 'GET'
+  })
+  .then((tweets) => {
+    renderTweets(tweets);
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+}
+
+$(document).ready(function() {
+  //grab the tweet container and store in jquery variable
+  const $tweets = ('.tweets');
+
+  loadTweets();
+
+  const $form = $('#submit-new-tweet');
+  
+  $form.on('submit', (event) => {
+    event.preventDefault();
+    if ($form.serialize() === 'text=') {
+      return alert("Please enter a tweet!");
+    }
+    if ($form.serialize().length - 5 > 140) {
+      return alert("You tweet is too damn long!");
+    }
+    const newTweetData = $form.serialize();
+
+    $.ajax({
+      method: 'POST',
+      url: '/tweets',
+      data: newTweetData
+    })
+    .then(() => {
+      loadTweets()
+      $('textarea').val("")
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  })
+
+
+});
 
