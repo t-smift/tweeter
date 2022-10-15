ced09ac8e85a7a3c54b9cee4d3f4697dcb956622
Taylor Smith <taylor.l.smith07@gmail.com>
2022-10-14 18:10:46 -0700

    fixed error messages and error styling

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,28 +1,56 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-const escape = function (str) {
-  let div = document.createElement("div");
-  div.appendChild(document.createTextNode(str));
-  return div.innerHTML;
-};
-//function to change the submitted tweet into "Safe Text"
+$(document).ready(function() {
 
-const createTweetElement = function (data) {
-  const tweetTime = timeago.format(data.created_at); 
-  const tweetBody = escape(data.content.text)
-  const result = `<article><header><span class="topTweetLine"><span class="tweeter-name"><img src="/images/profile-hex.png"><p>${data.user.name}</p></span><p>${data.user.handle}</p></span><p>${tweetBody}</p></header><footer class="tweetFeet"><span><h5>${tweetTime}</h5></span><span class="icons"><i class="fa-solid fa-flag"></i><i class="fa-solid fa-heart"></i><i class="fa-solid fa-retweet"></i></span></footer></article>`
-  return result;
-};
+  //this sequence hides the tweet submission, reveals it when the arrow is click and moves the cursor to the area
+  $('#submit-new-tweet').hide();
+  $('#arrow').on('click', (event) => {
+    $('#submit-new-tweet').slideToggle();
+    $('#tweet-text').focus();
+  });
 
-const renderTweets = function (tweets) {
-  for (let tweet of tweets) {
-    const $tweet = createTweetElement(tweet);
-    $('.tweets').prepend($tweet);
-  }
-};
+  loadTweets();
+
+  //grab the tweet container and store in jquery variable to be used in once page loads
+  const $tweets = ('.tweets');
+  const $form = $('#submit-new-tweet');
+  
+  const $errorContainer = $('.error');
+  $errorContainer.hide();
+
+  $form.on('submit', (event) => {
+    event.preventDefault();
+
+    //error for empty tweet
+    if ($form.serialize() === 'text=') {
+      $('#error-msg').text("You forgot to enter a tweet!")
+      $errorContainer.slideDown(2500);
+      $errorContainer.slideUp(1000);
+      return;
+    }
+
+    //error for too many characters
+    if ($form.serialize().length - 5 > 140) {
+      $errorContainer.slideDown(2500);
+      $('#error-msg').text("You tweet is too damn long!");
+      $errorContainer.slideUp(1000);
+      return;
+    }
+
+    const newTweetData = $form.serialize();
+    $.ajax({
+      method: 'POST',
+      url: '/tweets',
+      data: newTweetData
+    })
+    .then(() => {
+      loadTweets()
+      $('textarea').val("")
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  })
+});
+
 
 const loadTweets = function () {
   $.ajax({
@@ -37,59 +65,24 @@ const loadTweets = function () {
   })
 };
 
+//function to change the submitted tweet into "Safe Text"
+const escape = function (str) {
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
 
-$(document).ready(function() {
-  
- 
-  $('#submit-new-tweet').hide();
+const createTweetElement = function (data) {
+  const tweetTime = timeago.format(data.created_at); 
+  const tweetBody = escape(data.content.text);
+  const result = `<article><header><span class="topTweetLine"><span class="tweeter-name"><img src="/images/profile-hex.png"><p>${data.user.name}</p></span><p>${data.user.handle}</p></span><p>${tweetBody}</p></header><footer class="tweetFeet"><span><h5>${tweetTime}</h5></span><span class="icons"><i class="fa-solid fa-flag"></i><i class="fa-solid fa-heart"></i><i class="fa-solid fa-retweet"></i></span></footer></article>`
+  return result;
+};
 
-  $('#arrow').on('click', (event) => {
-    $('#submit-new-tweet').slideToggle();
-
-  });
-
-  loadTweets();
-
-  //grab the tweet container and store in jquery variable
-  const $tweets = ('.tweets');
-
-  const $form = $('#submit-new-tweet');
-  
-  const $errorContainer = $('.error1');
-  //selects the div element containing all the error html
-
- 
-
-  $form.on('submit', (event) => {
-    event.preventDefault();
-    if ($form.serialize() === 'text=') {
-      $('.error1').attr('class', 'error2'); 
-      //adds a class to the error container for styling
-
-      $errorContainer.slideDown(500);
-      $('#error-msg').text("You forgot to enter a tweet!")
-      
-      return;
-    }
-    if ($form.serialize().length - 5 > 140) {
-      $('#error-msg').text("You tweet is too damn long!");
-      return;
-    }
-    const newTweetData = $form.serialize();
-
-    $.ajax({
-      method: 'POST',
-      url: '/tweets',
-      data: newTweetData
-    })
-    .then(() => {
-      loadTweets()
-      $('textarea').val("")
-    })
-    .catch((err) => {
-      console.log(err)
-    })
-  })
-
-});
+const renderTweets = function (tweets) {
+  for (let tweet of tweets) {
+    const $tweet = createTweetElement(tweet);
+    $('.tweets').prepend($tweet);
+  }
+};
 
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
-  const $newTweet = $('.new-tweet textarea');
   
+  const $newTweet = $('.new-tweet textarea');
 
   $newTweet.on('input', (event) => {
     let count = event.target.value.length;
     let updatedCount = 140 - count;
+
     $('.new-tweet output').text(updatedCount);
     if (updatedCount <= 0) {
       $('.new-tweet output').attr('class', 'tooMany');
